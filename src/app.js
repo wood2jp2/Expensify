47dcaefd32410ff2ae1af548b99df480fa2a7bb4
Josh Wood <wood2jp2@gmail.com>
2018-11-25 21:47:52 -0500

    started redux store, etc

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'normalize.css/normalize.css'
-import './styles/styles.scss'
+import './Styles/styles.scss'
 
 import AppRouter from './Routers/AppRouter'
 
